a03b17cea5756a4a9a092ab929f9432c0de8742a
Stefano Verna <s.verna@cantierecreativo.net>
2016-11-19 17:03:23 +0100

    Done!

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,1 +1,21 @@
-// This is where it all goes :)
+var FldGrd = require('fld-grd');
+var baguetteBox = require('./baguettebox');
+
+var fldGrd = new FldGrd(document.querySelector('.gallery'), {
+  rowHeight: 250,
+  dataWidth: 'data-width',
+  dataHeight: 'data-height',
+});
+
+fldGrd.update();
+
+baguetteBox.run('.gallery', {
+  overlayBackgroundColor: 'white',
+});
+
+[].forEach.call(document.querySelectorAll('.js-toggle'), function(el) {
+  el.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('.main-nav').classList.toggle('is-active');
+  });
+});
