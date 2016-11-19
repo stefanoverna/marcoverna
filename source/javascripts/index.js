cf2d47766e44e214b04b16aec6380ea9ee3c8321
Stefano Verna <s.verna@cantierecreativo.net>
2016-11-19 17:27:44 +0100

    Fix

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -16,6 +16,18 @@ baguetteBox.run('.gallery', {
 [].forEach.call(document.querySelectorAll('.js-toggle'), function(el) {
   el.addEventListener('click', function(e) {
     e.preventDefault();
-    document.querySelector('.main-nav').classList.toggle('is-active');
+    var nav = document.querySelector('.main-nav');
+
+    if (nav.classList.contains('is-active')) {
+      nav.classList.remove('is-active-after');
+      setTimeout(function() {
+        nav.classList.remove('is-active');
+      }, 200);
+    } else {
+      nav.classList.add('is-active');
+      setTimeout(function() {
+        nav.classList.add('is-active-after');
+      }, 0);
+    }
   });
 });
