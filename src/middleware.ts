6cca13864195b2940dc27af43afab716aeaefb61
Stefano Verna <s.verna@datocms.com>
2026-08-10 13:13:31 +0200

    Add stale-while-revalidate to Cache-Control

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,9 @@ import { defineMiddleware } from 'astro:middleware';
 
 export const onRequest = defineMiddleware(async (_context, next) => {
   const response = await next();
-  response.headers.set('Cache-Control', 'public, max-age=60');
+  response.headers.set(
+    'Cache-Control',
+    'public, max-age=60, stale-while-revalidate=3600',
+  );
   return response;
 });
