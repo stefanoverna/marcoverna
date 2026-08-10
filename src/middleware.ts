ef66ed48d854cac8314a64e332e38c00707cf4eb
Stefano Verna <s.verna@datocms.com>
2026-08-10 17:51:12 +0200

    Fix cache

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-import { defineMiddleware } from 'astro:middleware';
 import { DATOCMS_BASE_EDITING_URL } from 'astro:env/server';
+import { defineMiddleware } from 'astro:middleware';
 import { isDraftModeEnabled } from '~/lib/draftMode';
 
 const baseEditingOrigin = new URL(DATOCMS_BASE_EDITING_URL).origin;
@@ -18,18 +18,15 @@ export const onRequest = defineMiddleware(async (context, next) => {
 
   const draft = await isDraftModeEnabled(context);
 
-  if (!draft) {
-    response.headers.set(
-      'Cache-Control',
-      'public, max-age=60, stale-while-revalidate=3600',
-    );
-  } else {
-    response.headers.set(
-      'Cache-Control',
-      'private, no-cache, no-store, must-revalidate',
-    );
-  }
-
+  response.headers.set(
+    'Cache-Control',
+    'private, no-cache, no-store, must-revalidate',
+  );
+  
+  // response.headers.set(
+  //   'Cache-Control',
+  //   'public, max-age=60, stale-while-revalidate=3600',
+  // );
 
   return response;
 });
