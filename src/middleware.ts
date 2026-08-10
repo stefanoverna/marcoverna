6b412f9c1e24a804f0cd584d21a40fe4299db1e7
Stefano Verna <s.verna@datocms.com>
2026-08-10 13:12:51 +0200

    Add 60s Cache-Control middleware

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+import { defineMiddleware } from 'astro:middleware';
+
+export const onRequest = defineMiddleware(async (_context, next) => {
+  const response = await next();
+  response.headers.set('Cache-Control', 'public, max-age=60');
+  return response;
+});
